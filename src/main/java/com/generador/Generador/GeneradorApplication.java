0e46481fc014f283c56319d1a3d4b9fda572d5b1
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-02-18 15:27:22 -0700

    initial commit

---
+++ src/main/java/com/generador/Generador/GeneradorApplication.java
@@ -0,0 +1,13 @@
+package com.generador.Generador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generador/Generador/GeneradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generador.Generador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
