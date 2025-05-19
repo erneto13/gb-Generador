51f55914aa9634c41acb6f74eca5dbb1a0166686
erneto13 <erneto.amaral13@gmail.com>
2025-05-18 21:18:15 -0700

    fix: update package names to lowercase in MainController

--- src/main/java/com/generador/Generador/controllers/MainController.java
+++ src/main/java/com/generador/Generador/controllers/MainController.java
@@ -1,14 +1,12 @@
-package com.generador.Generador.Controllers;
+package com.generador.Generador.controllers;
 
-import Util.ClassGenerator;
+import util.ClassGenerator;
 import org.json.JSONArray;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
